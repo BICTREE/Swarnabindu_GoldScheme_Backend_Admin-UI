096049786e65519e369df940e9d023a27acbb845
Abin S chandran <abinschandran1@gmail.com>
2026-07-09 12:37:48 +0530

    space added

--- routes/admin/adminAuthRoutes.js
+++ routes/admin/adminAuthRoutes.js
@@ -6,4 +6,6 @@ const { adminProtect, requireRole } = require('../../middleware/roleMiddleware')
 router.post('/login', adminAuthController.login);
 router.post('/create-admin', adminProtect, requireRole(['SUPER_ADMIN']), adminAuthController.createAdmin);
 
+
+
 module.exports = router;
